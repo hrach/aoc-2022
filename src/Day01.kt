99a0352744648a08c6bd53bca927a90dbd8b0b15
Jan Skrasek <hrach.cz@gmail.com>
2022-12-03 06:36:50 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,28 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+	fun part1(input: List<String>): Int {
+		val groups = mutableListOf<List<Int>>()
+		var group = mutableListOf<Int>()
+		input.forEach {
+			if (it.isBlank()) group = mutableListOf<Int>().also { groups.add(it) }
+			else group.add(it.toInt())
+		}
+		return groups.maxOf { it.sum() }
+	}
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+	fun part2(input: List<String>): Int {
+		val groups = mutableListOf<List<Int>>()
+		var group = mutableListOf<Int>()
+		input.forEach {
+			if (it.isBlank()) group = mutableListOf<Int>().also { groups.add(it) }
+			else group.add(it.toInt())
+		}
+		return groups.map { it.sum() }.sortedDescending().take(3).sum()
+	}
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+	val testInput = readInput("Day01_test")
+	check(part1(testInput), 24000)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+	val input = readInput("Day01")
+	println(part1(input))
+	println(part2(input))
 }
