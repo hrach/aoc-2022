8b93eaf104ad0b8bc13a65bfc667f0ab58d1949c
Jan Skrasek <hrach.cz@gmail.com>
2022-12-13 10:08:01 +0100

    add lcm fun

--- src/Utils.kt
+++ src/Utils.kt
@@ -14,6 +14,20 @@ fun check(actual: String, expected: String) {
     check(actual == expected) { "Check failed. Received $actual"}
 }
 
+fun lcm(n1: Int, n2: Int): Int {
+    var lcm = if (n1 > n2) n1 else n2
+
+    // Always true
+    while (true) {
+        if (lcm.rem(n1) == 0 && lcm.rem(n2) == 0) {
+            break
+        }
+        ++lcm
+    }
+
+    return lcm
+}
+
 /**
  * Reads lines from the given input txt file.
  */
